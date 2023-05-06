512f26509c1c04e5d519f4837ce0c049e9cd1cd6
Islam Gamal <eslam.gamal.younis@gmail.com>
2023-05-07 02:39:56 +0300

    init-repo

---
+++ EF_Core_6_Best_Practices_Solu/EF_Core_6_Best_Practices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_Core_6_Best_Practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
